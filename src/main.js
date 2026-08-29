502dac02f2f6098eb49489083e4a19dd3abf2ef0
Abhishek <abhi963411@gmail.com>
2026-08-29 22:35:52 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -170,11 +170,11 @@ document.addEventListener('DOMContentLoaded', () => {
       if (submitBtn) {
         submitBtn.disabled = true;
         submitBtn.innerHTML = `
-          <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-slate-950" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-slate-950" xmlns="https://script.google.com/macros/s/AKfycbyQkzOcXFCw6t8zrmId3hHKHkFTGZ7pZhX2pH6ukLjy43WIPy4QR5RtXAxaHL3di6HBSA/exec" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
             <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
             <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
           </svg>
-          <span>Sending to hello@feeldata.in...</span>
+          <span>Sending to founderabhishek@feeldata.in...</span>
         `;
       }
 
